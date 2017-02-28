286f8b8d3c5decd093537e2cb49e2ef4737dc8ea
“reddrum” <anton.kranz@me.com>
2017-02-28 23:23:44 +0300

    Added controller & model & some gems

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,19 @@
+class ItemsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params.require(:item).permit(:title, :description))
+
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+end
