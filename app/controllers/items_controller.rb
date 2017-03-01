1e1b459355c1f6cc0a26ed380b84eed1153b9193
“reddrum” <anton.kranz@me.com>
2017-03-01 23:13:31 +0300

    Added edit, create, show views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def show
 
   end
 
@@ -8,7 +14,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(params.require(:item).permit(:title, :description))
+    @item = Item.new(item_params)
 
     if @item.save
       redirect_to root_path
@@ -16,4 +22,31 @@ class ItemsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:title, :description)
+    end
+
+    def find_item
+      @item = Item.find(params[:id])
+    end
 end
